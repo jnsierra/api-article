13cce1f71f180863a9a01d7b16ddc04309ff9014
Jesus Sierra <jnsierrac@gmail.com>
2020-09-24 19:18:51 -0500

    Finalizacion del token de autenticacion

--- api-business/src/main/java/co/com/ud/business/controller/LoginController.java
+++ api-business/src/main/java/co/com/ud/business/controller/LoginController.java
@@ -2,6 +2,7 @@ package co.com.ud.business.controller;
 
 
 import co.com.ud.business.service.LoginService;
+import co.com.ud.business.service.TokenService;
 import co.com.ud.utiles.dto.TokenDto;
 import co.com.ud.utiles.dto.UsuarioDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,27 +14,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 
     private final LoginService loginService;
+    private final TokenService tokenService;
 
     @Autowired
-    public LoginController(LoginService loginService) {
+    public LoginController(LoginService loginService, TokenService tokenService) {
         this.loginService = loginService;
+        this.tokenService = tokenService;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TokenDto> login(@RequestBody UsuarioDto usuarioDto) {
         Boolean validaLogin = loginService.validaLogin(usuarioDto.getCorreo(), usuarioDto.getContrasena());
-        if(Boolean.TRUE.equals(validaLogin)){
+        if (Boolean.TRUE.equals(validaLogin)) {
             //Genero el token en la aplicacion
+            Optional<TokenDto> token = tokenService.generateTokenUser(usuarioDto.getCorreo());
+            if (token.isPresent()) {
+                return new ResponseEntity<TokenDto>(token.get(), HttpStatus.OK);
+            }
 
-            return new ResponseEntity<TokenDto>(TokenDto.builder().token("Ya").build(), HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
-
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 }
--- api-business/src/main/java/co/com/ud/business/service/impl/TokenServiceImpl.java
+++ api-business/src/main/java/co/com/ud/business/service/impl/TokenServiceImpl.java
@@ -4,24 +4,35 @@ import co.com.ud.business.rest.client.UsuarioCliente;
 import co.com.ud.business.service.TokenService;
 import co.com.ud.utiles.dto.TokenDto;
 import co.com.ud.utiles.dto.UsuarioDto;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.TextCodec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TokenServiceImpl implements TokenService {
 
     private final UsuarioCliente usuarioCliente;
+    public static Integer seconds = Integer.valueOf("3600") ;
+    private final String secret;
 
     @Autowired
-    public TokenServiceImpl(UsuarioCliente usuarioCliente) {
+    public TokenServiceImpl(UsuarioCliente usuarioCliente,@Value("${jwt.secret}")String secret) {
         this.usuarioCliente = usuarioCliente;
+        this.secret = secret;
     }
 
     @Override
@@ -32,13 +43,17 @@ public class TokenServiceImpl implements TokenService {
             Instant now = Instant.now();
 
 
-            /*jwt = Jwts.builder().setSubject(email)
+            jwt = Jwts.builder().setSubject(email)
                     .claim("authorities",
-                            grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                            getRolesByUser(email).stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                     .setIssuedAt(Date.from(now)).setExpiration(Date.from(now.plus(seconds, ChronoUnit.SECONDS)))
                     .signWith(SignatureAlgorithm.HS512, TextCodec.BASE64.encode(secret)).compact();
-            return jwt;*/
 
+            return Optional.of(TokenDto.builder()
+                    .token(jwt)
+                    .mensaje("Autenticacion exitosa")
+                    .time(seconds)
+                    .build());
         }
         return Optional.empty();
     }
@@ -57,6 +72,8 @@ public class TokenServiceImpl implements TokenService {
     }
 
     private List<GrantedAuthority> getRolesByUser(String email){
-        return null;
+        String roleStr = "ROLE_ADMIN";
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList(roleStr);
+        return grantedAuthorities;
     }
 }
