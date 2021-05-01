80ba3036c745b89fc2bd197b795660ac46fc340a
Jesus Sierra <jnsierrac@gmail.com>
2021-05-01 14:30:57 -0500

    Ajuste para bug al cargar un word

--- api-business/src/main/java/co/com/ud/business/service/impl/FormatoIdeaServiceImpl.java
+++ api-business/src/main/java/co/com/ud/business/service/impl/FormatoIdeaServiceImpl.java
@@ -5,6 +5,9 @@ import co.com.ud.business.service.FormatoIdeaService;
 import co.com.ud.business.service.IdeaService;
 import co.com.ud.utiles.dto.FormatoIdeaDto;
 import co.com.ud.utiles.service.UtilesBase64;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.checkerframework.checker.nullness.Opt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,8 @@ public class FormatoIdeaServiceImpl implements FormatoIdeaService {
     private final FormatoIdeaCliente formatoIdeaCliente;
     private final IdeaService ideaService;
     private String pathRepo;
+
+    private static final Logger logger = LogManager.getLogger(FormatoIdeaServiceImpl.class);
 
     @Autowired
     public FormatoIdeaServiceImpl(FormatoIdeaCliente formatoIdeaCliente,@Value("${path.repo.idea}")  String pathRepo, IdeaService ideaService) {
@@ -39,18 +44,34 @@ public class FormatoIdeaServiceImpl implements FormatoIdeaService {
     @Override
     public Optional<FormatoIdeaDto> persistirFormatoIdea(String token, FormatoIdeaDto formatoIdea) {
         //Guardo el archivo en el repo
-        String name = formatoIdea.getIdIdea() + "_idea." + formatoIdea.getTipo();
-        Boolean guardar = UtilesBase64.builder().build().saveFile(formatoIdea.getBase64(),pathRepo, name);
+        Optional<String> name = this.generoNombreDocumento(token, formatoIdea.getIdIdea(), formatoIdea.getFormato(), formatoIdea.getTipo());
+        if(name.isEmpty()){
+            logger.error("ERROR_SAVE_FORMATO|{}|{}|{}","IDEA",formatoIdea.getIdIdea() , formatoIdea.getFormato());
+            return Optional.empty();
+        }
+        Boolean guardar = UtilesBase64.builder().build().saveFile(formatoIdea.getBase64(),pathRepo, name.get());
         if(guardar){
             //cambiamos la ruta en la que se almaceno el archivo
             formatoIdea.setUbicacion(pathRepo + name);
-            formatoIdea.setNombre(name);
+            formatoIdea.setNombre(name.get());
             Optional<FormatoIdeaDto> formato = saveFormato(token,formatoIdea);
             if(formato.isPresent()){
                 //Actualiza el estado del idea
                 ideaService.updateStatus(token, formatoIdea.getIdIdea(),"POR_CONFIRMAR_FORMATO");
             }
             return formato;
+        }
+        return Optional.empty();
+    }
+
+    private Optional<String> generoNombreDocumento(String token,  Long idIdea, String tipoFormato, String tipoDocumento){
+        String name = tipoFormato + "_" + idIdea.toString() + "_idea";
+        //consulto si el usuario tiene mas versiones del docuemento
+        ResponseEntity<FormatoIdeaDto[]> response = formatoIdeaCliente.getFormatoByIdIdea(token,idIdea);
+        if(HttpStatus.OK.equals(response.getStatusCode())){
+            return Optional.of( name + "_" +response.getBody().length + "." + tipoDocumento );
+        }else if(HttpStatus.NO_CONTENT.equals(response.getStatusCode())){
+            return Optional.of( name + "." + tipoDocumento );
         }
         return Optional.empty();
     }
--- api-business/src/test/java/co/com/ud/business/service/impl/FormatoIdeaServiceImplTest.java
+++ api-business/src/test/java/co/com/ud/business/service/impl/FormatoIdeaServiceImplTest.java
@@ -55,7 +55,7 @@ public class FormatoIdeaServiceImplTest {
     }
 
     @Test
-    public void testPersistirFormatoIdeaSUCCESS()throws FileNotFoundException {
+    public void testPersistirFormatoIdeaSUCCESS_FIRST_VERSION()throws FileNotFoundException {
         ClassLoader classLoader = this.getClass().getClassLoader();
         File file = new File(classLoader.getResource("base64Text.txt").getFile());
         Scanner sc = new Scanner(file);
@@ -65,19 +65,60 @@ public class FormatoIdeaServiceImplTest {
         FormatoIdeaDto responseDto = FormatoIdeaDto
                 .builder()
                 .id(1L)
-                .formato("FORMATO_FIRMADO")
+                .formato("PROCESO_AUT_TUTOR")
+                .tipo("docx")
                 .idIdea(1l)
                 .nombre("FORMATO")
                 .base64(base64)
                 .build();
 
-        Mockito.doReturn(new ResponseEntity<>(responseDto, HttpStatus.OK)).when(formatoIdeaCliente).save(Mockito.any(), Mockito.any());
+        Mockito.doReturn(new ResponseEntity<>(HttpStatus.NO_CONTENT)).when(formatoIdeaCliente).getFormatoByIdIdea(Mockito.any(),Mockito.any());
 
+        Mockito.doReturn(new ResponseEntity<>(responseDto, HttpStatus.OK)).when(formatoIdeaCliente).save(Mockito.any(), Mockito.any());
 
         Optional<FormatoIdeaDto> response = formatoIdeaService.persistirFormatoIdea("1321546fadf",responseDto);
         Assert.assertNotNull(response);
         Assert.assertEquals(Boolean.TRUE, response.isPresent());
     }
 
+    @Test
+    public void testPersistirFormatoIdeaSUCCESS_SECOND_VERSION()throws FileNotFoundException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        File file = new File(classLoader.getResource("base64Text.txt").getFile());
+        Scanner sc = new Scanner(file);
+        String base64 = sc.next();
+
+        FormatoIdeaDto[] list = new FormatoIdeaDto[1];
+        FormatoIdeaDto responseEntity = FormatoIdeaDto
+                .builder()
+                .id(1L)
+                .formato("PROCESO_AUT_TUTOR")
+                .tipo("docx")
+                .idIdea(1l)
+                .nombre("FORMATO")
+                .base64(base64)
+                .build();
+        list[0] = responseEntity;
+
+
+
+        Mockito.doReturn(new ResponseEntity<>(list,HttpStatus.OK)).when(formatoIdeaCliente).getFormatoByIdIdea(Mockito.any(),Mockito.any());
+
+
+
+        Mockito.doReturn(new ResponseEntity<>(responseEntity, HttpStatus.OK)).when(formatoIdeaCliente).save(Mockito.any(), Mockito.any());
+
+        FormatoIdeaDto entity = FormatoIdeaDto
+                .builder()
+                .formato("PROCESO_AUT_TUTOR")
+                .tipo("docx")
+                .idIdea(1l)
+                .nombre("FORMATO")
+                .base64(base64)
+                .build();
+        Optional<FormatoIdeaDto> response = formatoIdeaService.persistirFormatoIdea("1321546fadf",entity);
+        Assert.assertNotNull(response);
+        Assert.assertEquals(Boolean.TRUE, response.isPresent());
+    }
 
 }
